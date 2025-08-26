75613880bb378652f97f0d342fefe3661672da74
Eric Pedley <ericpedley@gmail.com>
2025-08-25 22:06:09 -0700

    copy digit stuff and sub in kbot joint names and configuration

---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/loco_manipulation/tracking/config/kbot/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+gym.register(
+    id="Isaac-Tracking-LocoManip-KBot-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+    "env_cfg_entry_point": f"{__name__}.loco_manip_env_cfg:KBotLocoManipEnvCfg",
+    "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:KBotLocoManipPPORunnerCfg",
+    },
+)
+
+
+gym.register(
+    id="Isaac-Tracking-LocoManip-KBot-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+    "env_cfg_entry_point": f"{__name__}.loco_manip_env_cfg:KBotLocoManipEnvCfg_PLAY",
+    "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:KBotLocoManipPPORunnerCfg",
+    },
+)
