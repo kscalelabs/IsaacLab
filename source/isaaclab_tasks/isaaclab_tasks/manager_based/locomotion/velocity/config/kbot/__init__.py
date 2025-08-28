d6ae44539cbbe96e019e909407af9b4adf0ef690
Eric Pedley <ericpedley@gmail.com>
2025-08-27 17:43:27 -0700

    use rnn env for default PPO kbot config

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/kbot/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/kbot/__init__.py
@@ -10,7 +10,7 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:KBotRoughEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.rough_rnn_env_cfg:KBotRoughEnvCfg", # NOTE: this is the same as the RNN one now
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:KBotRoughPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
     },
