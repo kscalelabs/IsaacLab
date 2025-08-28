13ddaa2b02571a80e980f4e87d7d23ccf4abfe1d
Eric Pedley <ericpedley@gmail.com>
2025-08-28 16:35:45 -0700

    fix event cfg name

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/kbot/rough_rnn_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/kbot/rough_rnn_env_cfg.py
@@ -29,7 +29,7 @@ from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
 from isaaclab_assets import KBOT_CFG
 from isaaclab_tasks.manager_based.locomotion.velocity.velocity_env_cfg import (
     LocomotionVelocityRoughEnvCfg,
-    EventsCfg as VelocityEnvEventsConfig,
+    EventCfg as VelocityEnvEventConfig,
     RewardsCfg,
 )
 from isaaclab.envs import ManagerBasedRLEnv
@@ -838,7 +838,7 @@ class KBotCurriculumCfg:
     )
 
 @configclass
-class KBotEventsCfg(VelocityEnvEventsConfig):
+class KBotEventsCfg(VelocityEnvEventConfig):
     apply_force_torque: EventTerm | None = None
 
 
@@ -985,20 +985,20 @@ class KBotRoughEnvCfg_PLAY(KBotRoughEnvCfg):
         super().__post_init__()
 
         # make a smaller scene for play
-        self.scene.num_envs = 50
-        self.scene.env_spacing = 2.5
-        self.episode_length_s = 40.0
+        self.scene.num_envs = 1
+        self.scene.env_spacing = 0
+        self.episode_length_s = 600.0
         # spawn the robot randomly in the grid (instead of their terrain levels)
         self.scene.terrain.max_init_terrain_level = None
         # reduce the number of terrains to save memory
         if self.scene.terrain.terrain_generator is not None:
-            self.scene.terrain.terrain_generator.num_rows = 5
-            self.scene.terrain.terrain_generator.num_cols = 5
+            self.scene.terrain.terrain_generator.num_rows = 1
+            self.scene.terrain.terrain_generator.num_cols = 1
             self.scene.terrain.terrain_generator.curriculum = False
 
-        self.commands.base_velocity.ranges.lin_vel_x = (-1.5, 1.5)
-        self.commands.base_velocity.ranges.lin_vel_y = (-1.5, 1.5)
-        self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.0)
+        self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
+        self.commands.base_velocity.ranges.ang_vel_z = (0.0, 0.0)
         self.commands.base_velocity.ranges.heading = (0.0, 0.0)
         # disable randomization for play
         self.observations.policy.enable_corruption = False
