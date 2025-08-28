d1c455f183032b2da297729ca15741df400dfe46
Eric Pedley <ericpedley@gmail.com>
2025-08-27 22:16:28 -0700

    make play env

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/kbot/locomanip.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/kbot/locomanip.py
@@ -189,3 +189,37 @@ class KBotManipRoughEnvCfg(KBotRoughEnvCfg):
         # post init of parent
         super().__post_init__()
 
+
+@configclass 
+class KBotManipRoughEnvCfgPlay(KBotManipRoughEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+
+        # make a smaller scene for play
+        self.scene.num_envs = 1
+        self.scene.env_spacing = 0
+        self.episode_length_s = 600
+        # spawn the robot randomly in the grid (instead of their terrain levels)
+        self.scene.terrain.max_init_terrain_level = None
+        # reduce the number of terrains to save memory
+        if self.scene.terrain.terrain_generator is not None:
+            self.scene.terrain.terrain_generator.num_rows = 1
+            self.scene.terrain.terrain_generator.num_cols = 1
+            self.scene.terrain.terrain_generator.curriculum = False
+
+        self.commands.base_velocity.ranges.lin_vel_x = (-1.5, 1.5)
+        self.commands.base_velocity.ranges.lin_vel_y = (-1.5, 1.5)
+        self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
+        self.commands.base_velocity.ranges.heading = (0.0, 0.0)
+        # disable randomization for play
+        self.observations.policy.enable_corruption = False
+        # remove random pushing for play
+        self.events.base_external_force_torque = None
+        self.events.push_robot = None
+
+        # Disable push curriculum for play mode
+        self.curriculum.velocity_push_curriculum = None
+
+        # Disable domain randomization for play mode
+        self.curriculum.domain_randomize_curriculum = None
